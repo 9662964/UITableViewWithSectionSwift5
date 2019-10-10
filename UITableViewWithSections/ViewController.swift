31badaf48e5107631678995afe88023ad660fd62
Aman Aggarwal <aman@clickapps.co>
2019-10-10 23:32:09 +0530

    Initial Commit

---
+++ UITableViewWithSections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewWithSections
+//
+//  Created by Aman Aggarwal on 10/10/19.
+//  Copyright © 2019 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
